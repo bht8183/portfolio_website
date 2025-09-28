832d8add600065166576c898d7f2367e48ea5ce5
Brian Tokumoto <bht8183@g.rit.edu>
2025-09-28 00:21:46 -0400

    react vite tailwinds setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+export default function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-gray-900">
+      <div className="p-10 bg-red-500 text-white text-4xl rounded-lg shadow-lg">
+        Tailwind is working ✅
+      </div>
+    </div>
+  );
+}
