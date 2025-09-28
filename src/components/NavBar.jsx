ec40fd26c6e6d42d5538eb1494ff8dcb0665f58e
Brian Tokumoto <bht8183@g.rit.edu>
2025-09-28 01:33:16 -0400

    changed nav bar from sticky to fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,7 @@
 
 export default function NavBar() {
   return (
-    <nav className="sticky top-0 z-20 bg-gray-900/60">
+    <nav className="fixed top-0 left-0 w-full z-20 bg-gray-900/60">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
         <span className="text-2xl font-bold">Brian Tokumoto</span>
         <div className="space-x-6">
